fd77923e9e72d002481fa7ed15a61ee12ee81243
faceless49 <digital.zeus@mail.ru>
2022-12-07 10:38:06 +0300

    feat: P, TAG, Button comp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
+import React from 'react';
+import {Button, Htag, P, Tag} from '../components';
+
 export default function Home(): JSX.Element {
   return (
-    <div>
-
-    </div>
+    <>
+      <Htag tag="h1">Заголовок</Htag>
+      <Button appearance="primary" arrow="right">Кнопка</Button>
+      <Button appearance="ghost" arrow="down">Кнопка</Button>
+      <P size="l">Большой</P>
+      <P>Средний</P>
+      <P size="s">Маленький</P>
+      <Tag size="s">Ghost</Tag>
+      <Tag size="m" color="red">Red</Tag>
+      <Tag size="s" color="green">Green</Tag>
+      <Tag color="primary">Green</Tag>
+    </>
   );
 }
